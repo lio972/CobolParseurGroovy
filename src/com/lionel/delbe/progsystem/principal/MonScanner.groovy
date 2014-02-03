4806e62d6b7cf8ed8b43fdb743d61847581cc326
lio <progsystem@gmail.com>
2014-02-03 23:31:42 +0100

    ajout de regex mais bug apres ajout de call et link bizarre

--- src/com/lionel/delbe/progsystem/principal/MonScanner.groovy
+++ src/com/lionel/delbe/progsystem/principal/MonScanner.groovy
@@ -10,29 +10,54 @@ package com.lionel.delbe.progsystem.principal
 class MonScanner {
     File fichier = null
     Analyseur analyseur = null
+    def COBOL_INCLUDE = /INCLUDE\s+([\w\d]+)*([^\s*])*/
+    def COBOL_LINK = /LINK\s+PROGRAM\s*\(.+\)/
+    def COBOL_CALLS = /CALL\s+\(.+\)/
+
 
     MonScanner(File fichier) {
         this.fichier = fichier
     }
 
-    def private displayFichierEntree(){
+    def private displayFichierEntree() {
         println(fichier)
     }
 
 
-    def private afficherContenuFichier(){
-       println "debut de lecture"
-       fichier.eachLine {ligne -> println(ligne)}
+    def private afficherContenuFichier() {
+        println "debut de lecture"
+        fichier.eachLine { ligne ->
+            //println(ligne)
+            //rechercherLesCalls()
+            if (ligne =~ COBOL_CALLS) {
+                println ligne
+            }
+        }
 
     }
 
-    def private rechercherLesCalls(){
-           /*regular expression for  include\s+([\w\d]+)*([^\s*])
+    def private rechercherLesCalls() {
+        /*regular expression for  include\s+([\w\d]+)*([^\s*])*/
+        if (ligne =~ COBOL_INCLUDE) {
+            println ligne
+        }
+        if (ligne =~ COBOL_CALLS) {
+            println ligne
+        }
+        if (ligne =~ COBOL_LINK) {
+            println ligne
+        }
 
-           /**#regular expression for link  link\s+program\s*\(.+\)
-              EXEC  CICS  LINK  PROGRAM  ('MA90T07')
-                  COMMAREA (MATERCOM)
-            */
+        /** #regular expression for link  link\s+program\s*\(.+\)
+         * EXEC  CICS  LINK  PROGRAM  ('MA90T07')
+         *    COMMAREA (MATERCOM)
+         */
+
+        /**
+         *
+         */
+
+
     }
 
 
