9d469b1cba3fe7d4242d32c92a858767f88fb274
lio <progsystem@gmail.com>
2014-02-04 16:06:07 +0100

    ajout de regex mais bug apres ajout de call et link bizarre

--- src/com/lionel/delbe/progsystem/principal/MonScanner.groovy
+++ src/com/lionel/delbe/progsystem/principal/MonScanner.groovy
@@ -10,9 +10,13 @@ package com.lionel.delbe.progsystem.principal
 class MonScanner {
     File fichier = null
     Analyseur analyseur = null
-    def COBOL_INCLUDE = /INCLUDE\s+([\w\d]+)*([^\s*])*/
-    def COBOL_LINK = /LINK\s+PROGRAM\s*\(.+\)/
-    def COBOL_CALLS = /CALL\s+\(.+\)/
+    def private COBOL_INCLUDE = /INCLUDE\s+([\w\d]+)*([^\s*])*/
+    def private COBOL_LINK = /LINK\s+PROGRAM\s*\(.+\)/
+    def private COBOL_CALLS = /CALL\s+\(.+\)/
+    def ArrayList includes = new ArrayList()
+    def ArrayList links = new ArrayList()
+    def ArrayList calls = new ArrayList()
+
 
 
     MonScanner(File fichier) {
@@ -29,36 +33,35 @@ class MonScanner {
         fichier.eachLine { ligne ->
             //println(ligne)
             //rechercherLesCalls()
-            if (ligne =~ COBOL_CALLS) {
-                println ligne
-            }
+//            if (ligne =~ COBOL_INCLUDE && (ligne.charAt(6) != "*")) {
+//                println ligne
+//            }
+//        }
+//        if (ligne =~ COBOL_CALLS) {
+//            println ligne
+//        }
+//        if (ligne =~ COBOL_LINK) {
+//            println ligne
+            rechercherLesCalls(ligne)
         }
-
     }
 
-    def private rechercherLesCalls() {
+
+    def rechercherLesCalls(UneLigne) {
         /*regular expression for  include\s+([\w\d]+)*([^\s*])*/
-        if (ligne =~ COBOL_INCLUDE) {
-            println ligne
+        if (UneLigne =~ COBOL_INCLUDE && (UneLigne.charAt(6) != "*")) {
+            println UneLigne
+            def toto = UneLigne =~ COBOL_INCLUDE
+            println(toto)
         }
-        if (ligne =~ COBOL_CALLS) {
-            println ligne
+        if (UneLigne =~ COBOL_CALLS && (UneLigne.charAt(6) != "*")) {
+            println UneLigne
         }
-        if (ligne =~ COBOL_LINK) {
-            println ligne
+        if (UneLigne =~ COBOL_LINK && (UneLigne.charAt(6) != "*")) {
+            println UneLigne
         }
-
-        /** #regular expression for link  link\s+program\s*\(.+\)
-         * EXEC  CICS  LINK  PROGRAM  ('MA90T07')
-         *    COMMAREA (MATERCOM)
-         */
-
-        /**
-         *
-         */
 
 
     }
-
-
 }
+
